6a8871833f27bb36f2116e6bf6f3b7b040b39965
emmanuelgospel-dev <emmanuelgospel.ffc@gmail.com>
2026-07-29 10:55:03 +0100

    Switching to POSTGRES

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST', 'localhost'),
+        'PORT': os.getenv('PGPORT', '5432'),
     
     }
 }
